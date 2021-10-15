660cd932be95ccf1297ed03deb7d6902ec46a574
kimuyu <victorkimuyu@gmail.com>
2021-10-15 15:53:09 +0300

    Estimate Commit Created Estimate model and Estimate constituent models [DealerPart, OpenMarketPart, ContributionPart, OtherCost]
    On the Estimate models, I created @property getters and setters
    dealer_parts - For giving the total of DealerParts through the instance_of(DealerPart) filter method of PolymorphicModel class,
    ...

--- estimate/admin.py
+++ estimate/admin.py
@@ -3,31 +3,55 @@ from django.contrib import admin
 from estimate.models import DealerPart, OpenMarketPart, OtherCost, Estimate, ContributionPart
 
 
-@admin.register(DealerPart)
-class DealerPartAdmin(admin.ModelAdmin):
+# @admin.register(DealerPart)
+# class DealerPartAdmin(admin.ModelAdmin):
+#     readonly_fields = ['amount']
+#
+#
+# @admin.register(OpenMarketPart)
+# class OpenMarketPartAdmin(admin.ModelAdmin):
+#     list_display = ['description', 'quantity', 'unit_cost', 'negotiated', 'amount']
+#     readonly_fields = ['amount']
+#
+#
+# @admin.register(ContributionPart)
+# class ContributionPartAdmin(admin.ModelAdmin):
+#     fields = ['estimate', 'description', 'quantity', 'unit_cost', 'negotiated', 'contrib_perc', 'contrib_amount',
+#               'amount']
+#     readonly_fields = ['contrib_amount', 'amount']
+#     list_display = ['description', 'amount']
+#
+#
+# @admin.register(OtherCost)
+# class OtherCostAdmin(admin.ModelAdmin):
+#     list_display = ['description', 'amount']
+
+
+class DealerPartInline(admin.TabularInline):
+    model = DealerPart
+    fields = ['description', 'quantity', 'unit_cost', 'amount']
     readonly_fields = ['amount']
 
 
-@admin.register(OpenMarketPart)
-class OpenMarketPartAdmin(admin.ModelAdmin):
-    list_display = ['description', 'quantity', 'unit_cost', 'negotiated', 'amount']
+class OpenMarketPartInline(admin.TabularInline):
+    model = OpenMarketPart
+    fields = ['description', 'quantity', 'unit_cost', 'negotiated', 'amount']
     readonly_fields = ['amount']
 
 
-@admin.register(ContributionPart)
-class ContributionPartAdmin(admin.ModelAdmin):
-    fields = ['estimate', 'description', 'quantity', 'unit_cost', 'negotiated', 'contrib_perc', 'contrib_amount',
-              'amount']
-    readonly_fields = ['contrib_amount', 'amount']
-    list_display = ['description', 'amount']
+class ContributionPartInline(admin.TabularInline):
+    model = ContributionPart
+    fields = ['description', 'quantity', 'unit_cost', 'negotiated', 'contrib_perc', 'amount']
+    readonly_fields = ['amount']
 
 
-@admin.register(OtherCost)
-class OtherCostAdmin(admin.ModelAdmin):
-    list_display = ['description', 'amount']
+class OtherCostInline(admin.TabularInline):
+    model = OtherCost
+    fields = ['description', 'amount']
 
 
 @admin.register(Estimate)
 class EstimateAdmin(admin.ModelAdmin):
-    list_display = ['report', 'dealer_parts', 'open_market_parts', 'other_costs', 'contribution_parts',
+    inlines = [DealerPartInline, OpenMarketPartInline, ContributionPartInline, OpenMarketPartInline, OtherCostInline]
+    list_display = ['report', 'dealer_parts', 'open_market_parts', 'contribution_parts', 'other_costs',
                     'vat', 'estimate_total']
